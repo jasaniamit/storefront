95aaf8a43829b15a6f81676da715f34618d13ee2
jasaniamit <67775072+jasaniamit@users.noreply.github.com>
2026-04-22 06:31:04 +0530

    Update layout.tsx

--- src/app/[country]/[locale]/(storefront)/layout.tsx
+++ src/app/[country]/[locale]/(storefront)/layout.tsx
@@ -52,7 +52,6 @@ export default async function StorefrontLayout({
 
   return (
     <>
-      <AnnouncementBar />
       <Header
         rootCategories={rootCategories}
         basePath={basePath}
