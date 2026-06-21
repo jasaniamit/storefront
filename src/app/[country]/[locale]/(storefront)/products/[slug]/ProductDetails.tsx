efd79079a8c8487ca0afbbda1b5e5883a4d65ab8
jasaniamit <67775072+jasaniamit@users.noreply.github.com>
2026-06-21 08:11:06 +0530

    Update font class for product name heading

--- src/app/[country]/[locale]/(storefront)/products/[slug]/ProductDetails.tsx
+++ src/app/[country]/[locale]/(storefront)/products/[slug]/ProductDetails.tsx
@@ -186,7 +186,7 @@ export function ProductDetails({ product, basePath }: ProductDetailsProps) {
 
         {/* Product Info */}
         <div className="flex flex-col">
-          <h1 className="text-4xl font-normal text-gray-900 mb-2 leading-tight">
+          <h1 className="font-google-sans text-4xl font-normal text-gray-900 mb-2 leading-tight">
             {product.name}
           </h1>
 
