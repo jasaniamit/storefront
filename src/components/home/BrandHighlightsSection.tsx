257544eae91ed8a9993cfb4b9cbe2ff17cef9e5c
jasaniamit <67775072+jasaniamit@users.noreply.github.com>
2026-06-25 01:47:01 +0530

    Update BrandHighlightsSection.tsx

--- src/components/home/BrandHighlightsSection.tsx
+++ src/components/home/BrandHighlightsSection.tsx
@@ -78,7 +78,6 @@ export function BrandHighlightsSection() {
                                 className="w-full max-w-[700px] h-auto"
                                 unoptimized
                             />
-                        </div>
                     </div>
                 </div>
 
