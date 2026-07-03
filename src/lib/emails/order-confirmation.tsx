108def90aa7e4a229aa857e74bd2f04844138ea8
jasaniamit <67775072+jasaniamit@users.noreply.github.com>
2026-07-03 16:58:30 +0530

    Update order-confirmation.tsx

--- src/lib/emails/order-confirmation.tsx
+++ src/lib/emails/order-confirmation.tsx
@@ -86,7 +86,7 @@ export function OrderConfirmationEmail({
               src="https://www.nozfragrances.com/noz.png"
               alt="NOZ Fragrances"
               width={120}
-              height={auto}
+              height="auto"
               style={logo}
             />
           </Section>
