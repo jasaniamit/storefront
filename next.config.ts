f1c6a068c52c861a1c3147957f13edb11870bfc4
jasaniamit <67775072+jasaniamit@users.noreply.github.com>
2026-07-13 06:28:06 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,7 @@ const nextConfig: NextConfig = {
     },
   },
   images: {
+    minimumCacheTTL: 31536000,
     qualities: [25, 50, 75, 85, 100],
     dangerouslyAllowLocalIP: true,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
